54a31198dd76732f6a8bfc7e77f68d6320205fd5
Artem Sharipov <sharipovartem@github.com>
2024-02-06 01:37:08 -0500

    Basic app layout

--- dashboard.py
+++ dashboard.py
@@ -6,4 +6,11 @@ import warnings
 warnings.filterwarnings("ignore")
 
 st.set_page_config(page_title="Chess Openings Dashboard", layout="wide", page_icon=":chess_pawn:")
+st.markdown("<style>div.block-container {padding-top: 2rem;}</style>", unsafe_allow_html=True)
 st.title("Chess Opening Dashboard")
+st.selectbox(label="Select time column", options=["Blitz", "Bullet", "Rapid", "All"])
+st.selectbox(label="Select chess website", options=["chess.com", "lichess.org"])
+
+st.text_input(label="Enter your username")
+st.text("OR")
+st.file_uploader(label="Upload PGN", type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
